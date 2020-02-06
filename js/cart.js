0273ee70a5180748fad0b376c3eff8d29b93623a
andy19910102 <andy19910102@gmail.com>
2020-02-06 12:48:14 +0800

    Project init

---
+++ js/cart.js
@@ -0,0 +1,31 @@
+
+function Cart() {
+    // localStorage key
+    this.key = 'example-cart';
+    // 購物車的資料
+    this.data = [];
+    // 初始化購物車
+    this.initCart = function () {
+
+    }
+    // 傳入商品id與數量並新增商品至購物車
+    this.addItem = function (pid, amount) {
+
+    }
+    // 至購物車刪除於購物車內指定索引商品
+    this.deleteItem = function (i) {
+
+    }
+    // 清空購物車
+    this.emptyCart = function () {
+
+    }
+    // 更新資料到localStorage
+    this.updateDataToStorage = function () {
+
+    }
+    // 渲染購物車
+    this.render = function () {
+
+    }
+}
---
+++ js/products.js
@@ -0,0 +1,72 @@
+const products = [
+    {
+        id: '1',
+        title: '產品一',
+        price: 10,
+        img: 'https://picsum.photos/id/999/1200/600',
+        isAvailable: true
+    },
+    {
+        id: '2',
+        title: '產品二',
+        price: 60,
+        img: 'https://picsum.photos/id/1070/1200/600',
+        isAvailable: true
+    },
+    {
+        id: '3',
+        title: '產品三',
+        price: 180,
+        img: 'https://picsum.photos/id/1071/1200/600',
+        isAvailable: true
+    },
+    {
+        id: '4',
+        title: '產品四',
+        price: 220,
+        img: 'https://picsum.photos/id/1072/1200/600',
+        isAvailable: true
+    },
+    {
+        id: '5',
+        title: '產品五',
+        price: 360,
+        img: 'https://picsum.photos/id/1073/1200/600',
+        isAvailable: true
+    },
+    {
+        id: '6',
+        title: '產品六',
+        price: 360,
+        img: 'https://picsum.photos/id/1074/1200/600',
+        isAvailable: true
+    },
+    {
+        id: '7',
+        title: '產品七',
+        price: 400,
+        img: 'https://picsum.photos/id/1075/1200/600',
+        isAvailable: true
+    },
+    {
+        id: '8',
+        title: '產品八',
+        price: 450,
+        img: 'https://picsum.photos/id/1076/1200/600',
+        isAvailable: true
+    },
+    {
+        id: '9',
+        title: '產品九',
+        price: 520,
+        img: 'https://picsum.photos/id/1077/1200/600',
+        isAvailable: true
+    }
+];
+
+// 渲染商品
+function renderProducts() {
+
+}
+
+renderProducts();
